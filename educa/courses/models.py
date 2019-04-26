92eaebedc66fe2b69fab5c3378e736adf9f5bb76
Mohamed Darwish <mu.darwish23@gmail.com>
2019-04-26 18:58:15 +0200

    Defined Subject, Course, and Module models

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    """[summary]
+    Arguments:
+        models {[django]} -- [inheritance]
+    Returns:
+        [Subject] -- [A subject with a slug and title]
+    """
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    """
+    Course model that defines a complete course that is function
+    of a subject.
+    Arguments:
+        models {Django} -- Django models
+    """
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_names='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_lenght=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_new_add=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    """[summary]
+    Arguments:
+        models {[type]} -- [description]
+    """
+    course = models.ForeignKey(Course, related_name='Modules')
+    title = models.CharField(max_lenght=200)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+    

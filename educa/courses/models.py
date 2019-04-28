586ff407709d0ba7080f6e6017321dfcdf2357e7
Mohamed Darwish <mu.darwish23@gmail.com>
2019-04-28 14:01:23 +0200

    Defined ItemBase abstract class and class inheritance

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -54,3 +56,45 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    """
+    A generic Content model that links to Django ContentType and 
+    custom built module type.
+    Arguments:
+        models {Django} -- Django model builder
+    """
+    module = models.ForeignKey(Module, related_names='contents')
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name="%(class)s_related")
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        asbtract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    text = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Videos(ItemBase):
+    url = models.URLField()

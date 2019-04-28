452945f5a9e88af8e6132f3a92c3ed4224a51cc5
Mohamed Darwish <mu.darwish23@gmail.com>
2019-04-28 17:02:46 +0200

    Defined ModuleFormSet as an abstraction on top of formsets to handle related items

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'mine/$', views.ManageCourseListView.as_view(), 
-        name='manage_couse_list'),
+        name='manage_course_list'),
     url(r'create/$', views.CourseCreateView.as_view(), name='course_create'),
-    url(r'^)?P<pk>\d+)/edit/$', views.CourseUpdateView.as_view(), 
+    url(r'^(?P<pk>\d+)/edit/$', views.CourseUpdateView.as_view(), 
         name='course_edit'),
     url(r'^(?P<pk>\d+)/delete/$', views.CourseDeleteView.as_view(), 
         name='course_delete')

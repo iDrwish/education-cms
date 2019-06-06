d705b548bd275005d8e8482cf8dac34fcad3ad70
Mohamed Darwish <mu.darwish23@gmail.com>
2019-06-06 15:40:32 +0200

    Student registeration using UserRegiseration Modelform and registeration templates

---
+++ educa/students/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from . import views
+
+
+urlpatterns = [
+    url(
+        r'^register/$',
+        views.StudentRegisterationView.as_view(),
+        name='student_registeration'
+    )
+]

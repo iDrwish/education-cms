2c8716f029f91b873c051e3f520642fa6a525a2b
Mohamed Darwish <mu.darwish23@gmail.com>
2019-06-06 15:52:16 +0200

    Bug fix in student registeration

--- educa/students/views.py
+++ educa/students/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, reverse
-from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import View
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 class StudentRegisterationView(CreateView):
-    template_name = 'students/student/registeration.html'
+    template_name = 'students/student/registration.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('student_course_list')
 

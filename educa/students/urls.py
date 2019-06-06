8f377ac92276cc57551767dc563df95718b04d67
Mohamed Darwish <mu.darwish23@gmail.com>
2019-06-07 00:16:24 +0200

    Implemented memcache on API level for specific querysets, views, the entire site

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.views.decorators.cache import cache_page
 from . import views
 
 
@@ -20,12 +21,12 @@ urlpatterns = [
     ),
     url(
         r'^course/(?P<pk>\d+)/$',
-        views.StudentCourseDetailView.as_view(),
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail'
     ),
     url(
         r'^course/(?P<pk>\d+)/(?P<module_id>\d+)/$',
-        views.StudentCourseDetailView.as_view(),
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name='student_course_detail_module'
     ),
 ]
